5f6532ccafd93f91e44ffd859873ad41ef402899
mohamed-wageh <80712072+mohamed-wageh@users.noreply.github.com>
2023-06-13 12:29:37 +0200

    make it https

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from "react";
 import MovieCard from "./MovieCard";
 import "./App.css";
 import searchIcon from "./search.svg";
-const API_URL = "http://www.omdbapi.com?apikey=da9fa759";
+const API_URL = "https://www.omdbapi.com?apikey=da9fa759";
 const App = () => {
   const [movies, setMovies] = useState([]);
   const [search, setSearch] = useState("");
